8644daa04eba7a8978ba43eae06a0088f633777a
Sean Pivin <seanpivin@gmail.com>
2023-11-05 15:53:53 +0200

    loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute bg-slate-200/20 backdrop-blur-sm flex justify-center items-center inset-0">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
