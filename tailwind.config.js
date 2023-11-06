02b6d8dd3fe8e9809f73af1c23a38367d288629b
Sean Pivin <seanpivin@gmail.com>
2023-11-06 14:22:42 +0200

    adding fonts and hight to 100dvh for smart phones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      colors: {
+        "pizza-red": "#f44336",
+        "pizza-green": "#4caf50",
+        "pizza-yellow": "#ffeb3b",
+        "pizza-black": "#212121",
+        "pizza-grey": "#9e9e9e",
+        "pizza-white": "#fff",
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
