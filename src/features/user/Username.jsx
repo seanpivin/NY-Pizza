44156e0588cd5c11171e1b17f63942905dafe9b2
Sean Pivin <seanpivin@gmail.com>
2023-11-07 14:25:17 +0200

    redux is redy to go- userName is done

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) {
+    return null;
+  }
+
   return (
     <div className="text-sm hiddenn font-bold px-4 py-4 md:block flex items-center justify-between">
-      Sean Pivin
+      {username}
     </div>
   );
 }
