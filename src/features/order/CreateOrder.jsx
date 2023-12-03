8be32ff33ad98cbc2878ecfdc7525471eb037f0b
Sean Pivin <seanpivin@gmail.com>
2023-12-03 12:36:16 +0200

    adding the position to the user with redux thunk

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,5 @@
 import { Form, redirect, useActionData } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { createOrder } from "../../services/apiRestaurant";
 import { useNavigate } from "react-router-dom";
 import Btn from "../../ui/btn";
@@ -8,6 +9,7 @@ import { formatCurrency } from "../../utils/helpers";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
 import { useState } from "react";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -16,14 +18,26 @@ const isValidPhone = (str) =>
   );
 
 function CreateOrder() {
-  const username = useSelector((state) => state.user.username);
+  const [isPriority, setIsPriority] = useState(false);
+
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+
+  const isLoadingAddress = addressStatus === "loading";
+
   const navigate = useNavigate();
   const isSubmiting = navigate.state === "submitting";
-  const [isPriority, setIsPriority] = useState(false);
 
   const formErrors = useActionData();
 
   const cart = useSelector(getCart);
+
+  const dispatch = useDispatch();
 
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = isPriority ? totalCartPrice * 0.2 : 0;
@@ -51,23 +65,44 @@ function CreateOrder() {
           <div className="grow">
             <input type="tel" name="phone" required className="input w-full" />
             {formErrors?.phone && (
-              <p className=" text-xs mt-2 text-red-700 bg-red-100 rounded-md p-2">
+              <p className="text-xs mt-2 text-red-700 bg-red-100 rounded-md p-2">
                 {formErrors.phone}
               </p>
             )}
           </div>
         </div>
 
-        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
-          <label className=" sm:basis-40">Address</label>
+        <div className="relative mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
+              className="input w-full"
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
-              className="input w-full"
             />
+            {addressStatus === "error" && (
+              <p className="text-xs mt-2 text-red-700 bg-red-100 rounded-md p-2">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] z-50 top-[3px] md:right-[5px] md:top-[5px]">
+              <Btn
+                type="small"
+                disabled={isLoadingAddress}
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get Your Position
+              </Btn>
+            </span>
+          )}
         </div>
 
         <div className=" mb-12 flex items-center gap-5">
@@ -86,7 +121,16 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Btn disabled={isSubmiting} type="primary">
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.latitude && position.longitude
+                ? `${position.latitude},${position.longitude}`
+                : ""
+            }
+          />
+          <Btn disabled={isSubmiting || isLoadingAddress} type="primary">
             {isSubmiting
               ? "Placing order..."
               : `Order now for only ${formatCurrency(totalPrice)}`}
